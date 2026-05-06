dfe13c1633236d188885cf346acf19d6f9ae68a1
thientran-gif <thien.tran@bhdvn.com>
2026-05-06 16:36:51 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/yt-playlist/'
-    plugins: [react()],
+    base: '/yt-playlist/',
+    plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
